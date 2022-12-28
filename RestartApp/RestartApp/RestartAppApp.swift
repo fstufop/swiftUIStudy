a38c90060eeb8db62f4b50e848445eb0bd2b52d9
Filipe Simões Teodoro <68706037+fstufop@users.noreply.github.com>
2022-12-27 22:23:02 -0300

    🚀: First commit

---
+++ RestartApp/RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Filipe Simoes Teodoro on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
