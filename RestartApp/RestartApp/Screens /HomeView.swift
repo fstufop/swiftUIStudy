ecca9cb5d7112a7ee101e5a888163e0775203d9b
Filipe Simões Teodoro <68706037+fstufop@users.noreply.github.com>
2023-01-07 10:30:13 -0300

    🌟: Setup HomeView

--- RestartApp/RestartApp/Screens /HomeView.swift
+++ RestartApp/RestartApp/Screens /HomeView.swift
@@ -11,7 +11,43 @@ struct HomeView: View {
     @AppStorage(Strings.Defaults.onboarding) var isOnboardingVisible: Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            //Image
+            Spacer()
+            
+            ZStack {
+                CircleView(color: .gray, opacity: 0.2)
+                
+                Image(asset: Asset.Images.character2)
+                    .resizable()
+                    .scaledToFit()
+            }//: ZSTACK
+            
+            Text(Strings.Home.description)
+                .font(.system(.title3))
+                .foregroundColor(.secondary)
+                .fontWeight(.light)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Button {
+                withAnimation {
+                    isOnboardingVisible = true
+                }
+            } label: {
+                Image(systemName: Strings.SystemImage.Arrow.Triangle.Two.Circlepath.Circle.fill).imageScale(.large)
+                Text(Strings.Home.buttonTitle)
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            .tint(Color(asset: Asset.Colors.colorBlue))
+            
+        }//: VSTACK
+        .padding()
     }
 }
 
