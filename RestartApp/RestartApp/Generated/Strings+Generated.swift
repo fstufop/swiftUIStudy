a0c80c8048db295c0662b7c03f8bb888f8cdb813
Filipe Simões Teodoro <68706037+fstufop@users.noreply.github.com>
2023-01-07 10:29:58 -0300

    🧩: Update Localizable

--- RestartApp/RestartApp/Generated/Strings+Generated.swift
+++ RestartApp/RestartApp/Generated/Strings+Generated.swift
@@ -12,11 +12,39 @@ import Foundation
 internal enum Strings {
   /// RestartApp
   internal static let appName = Strings.tr("Localizable", "AppName", fallback: "RestartApp")
+  internal enum SystemImage {
+    internal enum Arrow {
+      internal enum Triangle {
+        internal enum Two {
+          internal enum Circlepath {
+            internal enum Circle {
+              /// arrow.triangle.2.circlepath.circle.fill
+              internal static let fill = Strings.tr("Localizable", "SystemImage.arrow.triangle.two.circlepath.circle.fill", fallback: "arrow.triangle.2.circlepath.circle.fill")
+            }
+          }
+        }
+      }
+    }
+    internal enum Chevron {
+      internal enum Right {
+        /// chevron.right.2
+        internal static let two = Strings.tr("Localizable", "SystemImage.chevron.right.two", fallback: "chevron.right.2")
+      }
+    }
+  }
   internal enum Defaults {
     /// onboarding
     internal static let onboarding = Strings.tr("Localizable", "defaults.onboarding", fallback: "onboarding")
   }
+  internal enum Home {
+    /// Restart
+    internal static let buttonTitle = Strings.tr("Localizable", "home.buttonTitle", fallback: "Restart")
+    /// The time that leads to mastery is dependent on intesity of our focus.
+    internal static let description = Strings.tr("Localizable", "home.description", fallback: "The time that leads to mastery is dependent on intesity of our focus.")
+  }
   internal enum Onboarding {
+    /// Get Started
+    internal static let buttonTitle = Strings.tr("Localizable", "onboarding.buttonTitle", fallback: "Get Started")
     /// It's not how much we give but
     /// how much love we put into giving.
     internal static let description = Strings.tr("Localizable", "onboarding.description", fallback: "It's not how much we give but\nhow much love we put into giving.")
