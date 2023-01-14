87e8f60bf3b629edf496ed188f9adf1df4379c4a
Filipe Simões Teodoro <68706037+fstufop@users.noreply.github.com>
2023-01-14 15:28:13 -0300

    🚀: Create ImageSizeControl

--- PinchApp/PinchApp/ContentView.swift
+++ PinchApp/PinchApp/ContentView.swift
@@ -21,50 +21,53 @@ struct ContentView: View {
         }
     }
     
+    func perfomMagnificent() {
+        if imageScale == 1 {
+            withAnimation {
+                imageScale = 5
+            }
+        } else {
+            resetImagePosition()
+        }
+    }
+    
     // MARK: - Content
     var body: some View {
         NavigationView {
             ZStack {
                 Color.clear
                 
-                Image(asset: Asset.Thumbnails.thumbMagazineFrontCover)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(10)
-                    .padding()
-                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
-                    .opacity(isAnimating ? 1 : 0)
-                    .offset(x: imageOffset.width, y: imageOffset.height)
-                    .scaleEffect(imageScale)
-                
-                // MARK: - TAP GESTURE
-                    .onTapGesture(count: 2) {
-                        if imageScale == 1 {
-                            withAnimation {
-                                imageScale = 5
-                            }
-                        } else {
-                            resetImagePosition()
+                ZStack {
+                    Image(asset: Asset.Thumbnails.thumbMagazineFrontCover)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .padding()
+                        .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(x: imageOffset.width, y: imageOffset.height)
+                        .scaleEffect(imageScale)
+                    
+                    // MARK: - TAP GESTURE
+                        .onTapGesture(count: 2) {
+                            perfomMagnificent()
                         }
-                    }
-                // MARK: - DRAG GESTURE
-                    .gesture(
-                        DragGesture()
-                            .onChanged { offset in
-                                withAnimation(.linear(duration: 1)) {
-                                    imageOffset = offset.translation
+                    // MARK: - DRAG GESTURE
+                        .gesture(
+                            DragGesture()
+                                .onChanged { offset in
+                                    withAnimation(.linear(duration: 1)) {
+                                        imageOffset = offset.translation
+                                    }
                                 }
-                            }
-                            .onEnded { _ in
-                                if imageScale <= 1 {
-                                    resetImagePosition()
+                                .onEnded { _ in
+                                    if imageScale <= 1 {
+                                        resetImagePosition()
+                                    }
                                 }
-                            }
-                    )
-                // MARK: - LONGPRESS GESTURE
-                    .gesture(
-                        LongPressGesture()
-                    )
+                        )
+                }//: ZSTACK
+                
             }//: ZSTACK
             .navigationTitle(Strings.navigationTitle)
             .navigationBarTitleDisplayMode(.inline)
@@ -73,12 +76,55 @@ struct ContentView: View {
                     isAnimating = true
                 }
             }//: ONAPPEAR
+            
+            // MARK: - INFO PAINEL
             .overlay(
                 InfoPainelView(scale: imageScale, offset: imageOffset)
                     .padding(.horizontal)
                     .padding(.top, 30)
                 , alignment: .top
             )
+            // MARK: - SIZE CONTROL
+            .overlay(
+                Group {
+                    HStack {
+                        Button {
+                            withAnimation(.spring()) {
+                                guard imageScale > 1 else { return }
+                                if imageScale > 0 {
+                                    imageScale -= 0.5
+                                }
+                            }
+                        } label: {
+                            ControllImageView(imageName: Strings.Magnifyingglass.minus)
+                        }
+                        
+                        Button {
+                            perfomMagnificent()
+                        } label: {
+                            ControllImageView(imageName: Strings.Magnifyingglass.scale)
+                        }
+                        
+                        Button {
+                            withAnimation(.spring()) {
+                                guard imageScale < 5 else { return }
+                                imageScale += 0.5
+                            }
+                        } label: {
+                            ControllImageView(imageName: Strings.Magnifyingglass.plus)
+                        }
+                        
+                    }//: HSTACK - CONTROLS
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    
+                }//: GROUP - CONTROLS
+                    .padding(.bottom, 30)
+                    .tint(.primary)
+                ,alignment: .bottom)
+            
         }//: NAVIGATION
         .navigationViewStyle(.stack)
     }
