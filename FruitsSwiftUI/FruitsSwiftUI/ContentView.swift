229fb086950ce366d998cf4a4c45b5de7b8d3627
filipe.teodoro <fstufop@gmail.com>
2023-01-25 19:15:59 -0300

    🚀: First commit

---
+++ FruitsSwiftUI/FruitsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FruitsSwiftUI
+//
+//  Created by filipe.teodoro on 25/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FruitsSwiftUI/FruitsSwiftUI/FruitsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsSwiftUIApp.swift
+//  FruitsSwiftUI
+//
+//  Created by filipe.teodoro on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
