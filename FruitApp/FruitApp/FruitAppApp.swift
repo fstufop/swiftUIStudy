a80f8e18322d2c66477e9a76778a8410bfcb7157
Filipe Simões Teodoro <68706037+fstufop@users.noreply.github.com>
2023-08-09 14:55:12 -0300

    Create Project

---
+++ FruitApp/FruitApp/FruitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitAppApp.swift
+//  FruitApp
+//
+//  Created by FilipeSTeodoro on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
